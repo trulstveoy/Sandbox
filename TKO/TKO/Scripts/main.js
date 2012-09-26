7f3a2c3e1aa6e962f1ef6f74de37802a4afcbd0b
Truls Tveoy <not@available>
2012-09-26 16:26:17 +0200

    stuffs

---
+++ TKO/TKO/Scripts/main.js
@@ -0,0 +1,20 @@
+﻿/*global define, require */
+(function () {
+    "use strict";
+    var root = this;
+
+    function defineDependencies() {
+        // These are already loaded via bundles. 
+        // We define them and put them in the root object.
+        define('jquery', [], function () { return root.jQuery; });
+        define('ko', [], function () { return root.ko; });
+        define('sammy', [], function () { return root.Sammy; });
+    }
+    
+    function boot() {
+        require(['bootstrapper'], function (bs) { bs.run(); });
+    }
+    
+    defineDependencies();
+    boot();
+})();
