e3032a6b277e708abdf2e9a8b7d276bd3f6bed7e
Truls Tveoy <not@available>
2012-09-27 09:30:35 +0200

    requirejs

---
+++ TKO/TKO/Scripts/app/bootstrapper.js
@@ -0,0 +1,15 @@
+﻿/*global define */
+/*jslint white: true */
+define('bootstrapper',
+    ['jquery', 'ko', 'sammy'],
+    function ($, ko, sammy) {
+        "use strict";
+
+        var run = function () {
+            debugger;
+        };
+
+        return {
+            run: run
+        };
+    });
--- TKO/TKO/Scripts/main.js
+++ TKO/TKO/Scripts/main.js
@@ -1,20 +1,23 @@
-﻿/*global define, require */
+﻿/*global define, require, requirejs */
+/*jslint sloppy: true white: true */
 (function () {
-    "use strict";
-    var root = this;
-
+    var root = this; //omit use strict to make this work
     function defineDependencies() {
-        // These are already loaded via bundles. 
-        // We define them and put them in the root object.
         define('jquery', [], function () { return root.jQuery; });
         define('ko', [], function () { return root.ko; });
         define('sammy', [], function () { return root.Sammy; });
     }
     
     function boot() {
-        require(['bootstrapper'], function (bs) { bs.run(); });
+        requirejs.config({
+            baseUrl: 'Scripts/app'
+        });
+
+        requirejs(['bootstrapper'], function (bs) {
+            bs.run();
+        });
     }
-    
+
     defineDependencies();
     boot();
 })();
