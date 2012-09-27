448c6dd1c861efbd398628d410a4c1c518ef1991
Truls Tveoy <not@available>
2012-09-27 20:54:04 +0200

    try to hook up viewmodels

--- TKO/TKO/Scripts/app/bootstrapper.js
+++ TKO/TKO/Scripts/app/bootstrapper.js
@@ -1,12 +1,15 @@
 ﻿/*global define */
 /*jslint white: true */
 define('bootstrapper',
-    ['jquery', 'ko', 'sammy'],
-    function ($, ko, sammy) {
+    ['jquery', 'ko', 'vm.shell'],
+    function ($, ko, viewModel) {
         "use strict";
 
         var run = function () {
             debugger;
+            var test = viewModel.name;
+            //enable shell
+            ko.applyBindings(viewModel, $("#shell-view")[0]);
         };
 
         return {
---
+++ TKO/TKO/Scripts/app/vm.shell.js
@@ -0,0 +1,13 @@
+﻿/*global define */
+/*jslint white: true */
+define('vm.shell',
+    ['ko', 'sammy'],
+    function(ko) {
+        "use strict";
+        
+        var name = ko.observable("test");
+
+        return {
+			name: name  
+        };
+	});
