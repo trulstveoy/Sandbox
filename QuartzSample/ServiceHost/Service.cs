d30a61a8a2b882ff245a4c86e3fe40f763bde5f4
Truls Tveoy <truls@notmyemail.com>
2013-02-08 11:48:16 +0100

    using ado jobstore

--- QuartzSample/ServiceHost/Service.cs
+++ QuartzSample/ServiceHost/Service.cs
@@ -1,4 +1,5 @@
-﻿using Quartz;
+﻿using System.Collections.Specialized;
+using Quartz;
 using Quartz.Impl;
 
 namespace ServiceHost
@@ -9,7 +10,18 @@ namespace ServiceHost
 
         public Service(string scheduleExpression)
         {
-            ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            var properties = new NameValueCollection();
+            properties.Add("quartz.jobStore.type", "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz");
+            properties.Add("quartz.jobStore.dataSource", "default");
+            properties.Add("quartz.jobStore.tablePrefix", "QRTZ_");
+            properties.Add("quartz.jobStore.clustered", "true");
+            properties.Add("quartz.jobStore.lockHandler.type", "Quartz.Impl.AdoJobStore.UpdateLockRowSemaphore, Quartz");
+            properties.Add("quartz.jobStore.driverDelegateType", "Quartz.Impl.AdoJobStore.SqlServerDelegate, Quartz");
+            properties.Add("quartz.dataSource.default.connectionString", "Server=localhost;Database=QuartzConfig;Uid=dbuser;Pwd=dbpass;");
+            properties.Add("quartz.dataSource.default.provider", "SqlServer-20");
+            properties.Add("quartz.jobStore.useProperties", "true");
+
+            ISchedulerFactory schedulerFactory = new StdSchedulerFactory(properties);
             _scheduler = schedulerFactory.GetScheduler();
             _scheduler.JobFactory = new JobFactory();
 
