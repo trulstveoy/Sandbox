727013428f67f666d3c89b4fa351f10709e88fee
Truls Tveoy <truls@notmyemail.com>
2013-02-12 13:13:41 +0100

    surr

---
+++ EmbeddedResourceWorksLikeThis/EmbeddedResourceWorksLikeThis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace EmbeddedResourceWorksLikeThis.LastName
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var manifestResourceName in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+            {
+                Console.WriteLine(manifestResourceName);
+            }
+
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("EmbeddedResourceWorksLikeThis.LastName.Foo.Bar.Text.txt");
+            var reader = new StreamReader(stream);
+            string text = reader.ReadToEnd();
+
+            Console.WriteLine(text);
+        }
+    }
+}
