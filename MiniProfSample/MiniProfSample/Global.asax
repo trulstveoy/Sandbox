aa30f1fc93c2878c4e50ce1e8ad5e16c07027c20
Truls Tveoy <a@a.aa>
2013-11-25 10:39:10 +0100

    miniprof

---
+++ MiniProfSample/MiniProfSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniProfSample.MvcApplication" Language="C#" %>
--- MiniProfSample/MiniProfSample/Global.asax.cs
+++ MiniProfSample/MiniProfSample/Global.asax.cs
@@ -1,25 +1,25 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using StackExchange.Profiling;
 
-namespace Webbox
+namespace MiniProfSample
 {
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
-            ConfigureCamelCaseJson();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            DurandalBundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
         protected void Application_BeginRequest()
@@ -30,15 +30,6 @@ namespace Webbox
         protected void Application_EndRequest()
         {
             MiniProfiler.Stop();
-        }
-
-        private static void ConfigureCamelCaseJson()
-        {
-            var formatters = GlobalConfiguration.Configuration.Formatters;
-            var jsonFormatter = formatters.JsonFormatter;
-            var settings = jsonFormatter.SerializerSettings;
-            settings.Formatting = Formatting.Indented;
-            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
