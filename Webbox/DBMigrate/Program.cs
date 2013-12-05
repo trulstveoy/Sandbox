35a59a79eda157fe5e463b37b5a87c1f70b85024
Truls Tveoy <a@a.aa>
2013-12-05 20:35:52 +0100

    abc

--- Webbox/DBMigrate/Program.cs
+++ Webbox/DBMigrate/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Webbox.DataAccess;
+﻿using Webbox.DataAccess;
 
 namespace DBMigrate
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Migrate.Now();
         }
