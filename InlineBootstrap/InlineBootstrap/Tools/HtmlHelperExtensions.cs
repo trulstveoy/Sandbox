7907f8c9b5a86cdacb6c01b68d68ff4dc942f8c8
Truls Tveoy <a@a.aa>
2014-02-25 06:26:02 +0100

    inline bootstrap

---
+++ InlineBootstrap/InlineBootstrap/Tools/HtmlHelperExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace InlineBootstrap.Tools
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString InlineScriptBlock<TModel>(this HtmlHelper<TModel> htmlHelper, string path)
+        {
+            var builder = new TagBuilder("script");
+            builder.Attributes.Add("type", "text/javascript");
+
+            var physicalPath = htmlHelper.ViewContext.RequestContext.HttpContext.Server.MapPath(path);
+            builder.InnerHtml = File.ReadAllText(physicalPath);
+
+            return MvcHtmlString.Create(builder.ToString());
+        }
+
+        public static MvcHtmlString InlineStylesheet<TModel>(this HtmlHelper<TModel> htmlHelper, string path)
+        {
+            var builder = new TagBuilder("style");
+
+            var physicalPath = htmlHelper.ViewContext.RequestContext.HttpContext.Server.MapPath(path);
+            builder.InnerHtml = File.ReadAllText(physicalPath);
+
+            return MvcHtmlString.Create(builder.ToString());
+        }
+    }
+
+}
