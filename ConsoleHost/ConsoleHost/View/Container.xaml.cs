3a9c17cb51e84d547c47b35f2d489a15a48a7eba
Truls Tveoy <a@a.aa>
2014-06-13 06:49:20 +0200

    a

--- ConsoleHost/ConsoleHost/View/Container.xaml.cs
+++ ConsoleHost/ConsoleHost/View/Container.xaml.cs
@@ -1,71 +1,28 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Interop;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace ConsoleHost.View
 {
     /// <summary>
     /// Interaction logic for Container.xaml
     /// </summary>
-    public partial class Container : HwndHost
+    public partial class Container : UserControl
     {
         public Container()
         {
             InitializeComponent();
-        }
-
-        private Process _process;
-        public string Path { get; set; }
-
-        [DllImport("user32.dll")]
-        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32")]
-        private static extern IntPtr SetParent(IntPtr hWnd, IntPtr hWndParent);
-
-        private const int GWL_STYLE = -16;
-        private const int WS_CAPTION = 0x00C00000;
-        private const int WS_THICKFRAME = 0x00040000;
-        private const int WS_CHILD = 0x40000000;
-
-        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
-        {
-            Path = @"..\..\..\TestApp\bin\debug\TestApp.exe";
-            if (string.IsNullOrWhiteSpace(Path)) throw new InvalidOperationException("Path cannot be null or whitespace");
-            _process = ProcessManager.StartProcess(Path);
-
-            var handle = _process.MainWindowHandle;
-
-            int style = GetWindowLong(handle, GWL_STYLE);
-            style = style & ~WS_CAPTION & ~WS_THICKFRAME;
-            style |= WS_CHILD;
-
-            SetWindowLong(handle, GWL_STYLE, style);
-            SetParent(handle, hwndParent.Handle);
-
-            InvalidateVisual();
-
-            var hwnd = new HandleRef(this, handle);
-            return hwnd;
-        }
-
-        protected override void DestroyWindowCore(HandleRef hwnd)
-        {
-            _process.CloseMainWindow();
-            _process.WaitForExit(5000);
-            if (_process.HasExited == false)
-            {
-                _process.Kill();
-            }
-
-            _process.Close();
-            _process.Dispose();
-            _process = null;
         }
     }
 }
